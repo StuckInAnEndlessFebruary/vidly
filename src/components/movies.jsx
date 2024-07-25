459cd684e30c630e5ba543c2b44b4873a4f47ed4
Mina <stuckinanendlessfebruary@gmail.com>
2024-07-25 13:41:34 +0330

    exercise 1 completed

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,69 @@
 import React, { Component } from "react";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
+
 class Movies extends Component {
-  state = {};
+  state = {
+    movies: getMovies(),
+  };
+
+  tableStyle = {
+    width: "100%",
+    margin: "30px",
+  };
+
+  btnStyle = {
+    margin: "10px",
+  };
+
+  handleDelete = (movieId) => {
+    const movies = this.state.movies.filter((movie) => movie._id !== movieId);
+    this.setState({ movies });
+  };
+
+  showParag(count) {
+    if (count) {
+      return `showing ${count} movies`;
+    } else {
+      return "no movies here";
+    }
+  }
+
   render() {
-    return <p>hi this works</p>;
+    return (
+      <div>
+        <p>{this.showParag(this.state.movies.length)}</p>
+        <table style={this.tableStyle}>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie._id)}
+                    style={this.btnStyle}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
